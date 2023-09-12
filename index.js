f2be8804b31a9397e433a8189371db760681e199
AbdiazizSaman <farhanadam394@gmail.com>
2023-09-12 21:07:30 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,50 @@
+function add(a,b){
+    return a+b ;
+
+}
+
+add(5,6);
+
+function subtract(a,b){
+    return a-b;
+
+}
+subtract(10,3);
+
+function multiply(a,b){
+    return a*b;
+
+}
+multiply(9.2);
+r
+function divide(a,b){
+    return a/b;
+
+}
+divide(9,3);
+
+
+function increment(n) {
+    return n + 1;
+  }
+  
+  increment();
+
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  decrement();
+
+  function makeInt(n){
+
+    return parseInt(n,10);
+    
+  }
+
+  function preserveDecimal(n){
+
+    return parseFloat(n, 10);
+  }
+
+  
